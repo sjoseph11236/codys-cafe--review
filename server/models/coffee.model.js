ace0515fd628d1c1d753d200c4d943d7882daf06
Sayeed Joseph <sayeedjoseph@gmail.com>
2020-01-30 09:48:59 -0500

    feat: add class method

--- server/models/coffee.model.js
+++ server/models/coffee.model.js
@@ -1,10 +1,34 @@
 const Sequelize = require('sequelize')
+
 const db = require('./database')
 
 const Coffee = db.define('coffee', {
-  name: Sequelize.STRING,
+  name:{ 
+    type: Sequelize.STRING, 
+    allowNull: false
+  },
   // Need to be an array WITH strings
   ingredients:  Sequelize.ARRAY(Sequelize.STRING)
 });
+
+Coffee.findByIngredient = async function(coffee) {
+
+  const Op = Sequelize.Op;
+  
+  const drinksWithCoffee = await Coffee.findAll({
+    where: { 
+      // Add the column for relevant for coffee
+      ingredients: {
+        [Op.contains]:[coffee]
+      }
+    }
+  })
+  
+  return drinksWithCoffee;
+};
+
+Coffee.prototype.getIngredients = function() {
+  return this.ingredients.join(', ');
+};
 
 module.exports = Coffee;
